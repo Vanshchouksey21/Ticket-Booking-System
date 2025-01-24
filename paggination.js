d8b5e64590f641ff0f6ff101b0d4e624ba26ac5c
Vanshchouksey21 <vanshchouksey2175@gmail.com>
2025-01-24 17:35:25 +0530

    final pj

--- paggination.js
+++ paggination.js
@@ -1,31 +1,31 @@
 
 
  
-  const swiper = new Swiper('.swiper-container', {
-    loop: true, // Make the carousel loop
-    slidesPerView: 3, // Display 3 movies at a time
-    spaceBetween: 20, // Space between slides
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true, // Allows click on pagination
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 10
-      },
-      480: {
-        slidesPerView: 1,
-        spaceBetween: 5
-      }
-    }
-  });
+  // const swiper = new Swiper('.swiper-container', {
+  //   loop: true, // Make the carousel loop
+  //   slidesPerView: 3, // Display 3 movies at a time
+  //   spaceBetween: 20, // Space between slides
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     clickable: true, // Allows click on pagination
+  //   },
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   },
+  //   breakpoints: {
+  //     1024: {
+  //       slidesPerView: 3,
+  //       spaceBetween: 20
+  //     },
+  //     768: {
+  //       slidesPerView: 2,
+  //       spaceBetween: 10
+  //     },
+  //     480: {
+  //       slidesPerView: 1,
+  //       spaceBetween: 5
+  //     }
+  //   }
+  // });
 
